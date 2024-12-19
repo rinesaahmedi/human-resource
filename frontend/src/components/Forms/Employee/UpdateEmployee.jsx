dddb816671aefd32ff72f86fbceeeb3f44e9686e
rinesaahmedi <rinesaahmedi03@gmail.com>
2024-12-19 10:14:52 +0100

    Sort imports

--- frontend/src/components/Forms/Employee/UpdateEmployee.jsx
+++ frontend/src/components/Forms/Employee/UpdateEmployee.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+
 import Input from "../../Input";
 
 const schema = yup.object().shape({
