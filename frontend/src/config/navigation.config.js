72a5668dcaa57445c8c86668825d2aa75d779f34
rinesaahmedi <149687156+rinesaahmedi@users.noreply.github.com>
2024-12-19 15:54:25 +0100

    Feature/review page (#21)
    * WIP: Started working on the reviews page
    
    * Setup review CRUD

--- frontend/src/config/navigation.config.js
+++ frontend/src/config/navigation.config.js
@@ -3,11 +3,17 @@ import { FaUsers } from "react-icons/fa6";
 import { TbReportSearch } from "react-icons/tb";
 import { FaRegUserCircle } from "react-icons/fa";
 import { BsFillBuildingsFill } from "react-icons/bs";
+import { MdRateReview } from "react-icons/md";
 
 export const configRoutes = [
   { path: "/users", icon: <HiOutlineUsers />, name: "Users" },
   { path: "/employees", icon: <FaUsers />, name: "Employees" },
   { path: "/department", icon: <BsFillBuildingsFill />, name: "Departments" },
+  {
+    path: "/reviews",
+    icon: <MdRateReview />,
+    name: "Reviews",
+  },
   { path: "/reports", icon: <TbReportSearch />, name: "Reports" },
   { path: "/my-profile", icon: <FaRegUserCircle />, name: "My Profile" },
 ];
