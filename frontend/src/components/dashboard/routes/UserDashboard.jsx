9bbc73d378c6290c118d081e93cc35398a098ce3
Shkurte <shkurtesalihu2@gmail.com>
2025-01-14 00:52:21 +0100

    added dashboard

---
+++ frontend/src/components/dashboard/routes/UserDashboard.jsx
@@ -0,0 +1,11 @@
+function UserDashboard() {
+    return (
+        <>
+        <h1 style={{color:'white'}}>
+            This is user Page
+        </h1>
+        </>
+    );
+}
+
+export default UserDashboard;
